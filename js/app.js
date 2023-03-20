44fbe53bdf287629f938ee4873be07d6facd7304
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-03-20 13:46:46 -0300

    feat: criando estrutura inicial e adicionando lógica para quebrar algoritmo e verificar ocorrencias de comandos

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const palavrasReservadas = [
+  "fimalgoritmo",
+  "algoritmo",
+  "inicio",
+  "var",
+  "inteiro",
+  "real",
+  "logico",
+  "caractere",
+  "cadeia",
+  "de",
+  "ate",
+  "fimpara",
+  "para",
+  "facaenquanto",
+  "faca",
+  "fimenquanto",
+  "enquanto",
+  "entao",
+  "senao",
+  "fimse",
+  "se",
+  "fimescolha",
+  "escolha",
+  "caso",
+  "fim",
+  "interrompa",
+  "retorne",
+  "leia",
+  "escreval",
+  "escreva",
+];
+
+/**
+ * Clear all string inside visualg algorithm
+ * @param {{item: String, times: Number}[]} occurrences
+ * */
+const mountTable = (occurrences) => {
+  const table = document.getElementById("table");
+  table.innerHTML = `${occurrences
+    .map(
+      (occurrence) =>
+        `<tr><td>${occurrence.item}</td><td>${occurrence.times}</td></tr>`
+    )
+    .join("")}`;
+};
+
+/**
+ * Clear all string inside visualg algorithm
+ * @param {String} code
+ * @returns {String}
+ * */
+const clearCode = (code) => {
+  const lowered = code.toLowerCase();
+  const withoutStrings = lowered.replaceAll(/"(.*?)"/g, "");
+  const withoutCommentaries = withoutStrings.replaceAll(/\/\/(.*?)\n/g, "");
+  return withoutCommentaries;
+};
+
+const analizeCode = () => {
+  const code = document.getElementById("codigo").value;
+  const occurrences = [];
+
+  let clearedCode = clearCode(code);
+
+  palavrasReservadas.forEach((palavra) => {
+    const occ = (clearedCode.match(new RegExp(palavra, "gi")) || []).length;
+    clearedCode = clearedCode.replaceAll(new RegExp(palavra, "gi"), "");
+    if (occ > 0) occurrences.push({ item: palavra, times: occ });
+  });
+
+  mountTable(occurrences);
+};
+
+document.getElementById("analize").addEventListener("click", analizeCode);
